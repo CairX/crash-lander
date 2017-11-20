cf26af66e46e30144486cbffe6b923f254315214
Thomas Cairns <lazycairx@gmail.com>
2017-11-20 11:03:10 +0100

    Rename script Terrain => Resources because Terrain is reserved name by Unity

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Terrain : MonoBehaviour {
+public class Resources : MonoBehaviour {
 
 	public Sprite Ship;
 	public Sprite Standard;
